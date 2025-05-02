46618bee3cf6c1ad33fc1b06a06f20a973812bb3
Akash <158745842+Akash8t2@users.noreply.github.com>
2025-05-02 22:15:06 +0530

    Update zowner.py

--- BrandrdXMusic/plugins/tools/zowner.py
+++ BrandrdXMusic/plugins/tools/zowner.py
@@ -11,13 +11,13 @@ from BrandrdXMusic.utils.database import add_served_chat, get_assistant
 @app.on_message(filters.command("repo"))
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/1aac9a42f6f35138da34b.jpg",
+        photo=f"https://graph.org/file/819d5ad9496dd446f94d7-eebd43e6d11f74b131.jpg",
         caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://github.com/Akash8t2/VIBEFXLOW"
+                        "ƨσʋяcɛ", url=f"https://t.me/SFW_BotCore"
                     )
                 ]
             ]
@@ -34,7 +34,7 @@ async def clones(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://github.com/Akash8t2/VIBEFXLOW"
+                        "ƨσʋяcɛ", url=f"https://t.me/SFW_BotCore"
                     )
                 ]
             ]
